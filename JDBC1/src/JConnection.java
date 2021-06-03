739eef5540d941c2b9d2210b78d9b67e9184323d
PIYUSH MALI <70318450+piyushmali@users.noreply.github.com>
2021-06-03 17:47:30 +0530

    Add files via upload

---
+++ JDBC1/src/JConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+public class JConnection {
+    public static Connection ConnecrDB(){
+        try{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/company","root","");   
+                System.out.println("Connected");
+                return conn;
+    }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+    
+     }
+   }
+}
